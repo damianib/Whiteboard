8bc2c21fc4ffe1926907506cc8d7829d997ade25
Benoit Damiani <damiani.benoit.jp@gmail.com>
2019-10-15 14:47:09 +0200

    Clean code of xaml Add new "do" functions

--- WhiteboardWPF/MainWindow.xaml.cs
+++ WhiteboardWPF/MainWindow.xaml.cs
@@ -1,45 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;
-using System.Net.Sockets;
-using System.Windows.Ink;
-using System.Runtime.InteropServices;
 
 
 namespace WhiteboardWPF
 {
-    
+
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         List<String> availableColorsStr = new List<String>() { "Black", "Red", "Green", "Blue" };
-        List<Color> availableColors = new List<Color>() { Color.FromRgb(0, 0, 0), Color.FromRgb(255, 0, 0), Color.FromRgb(0, 255, 0), 
+        List<Color> availableColors = new List<Color>() { Color.FromRgb(0, 0, 0), Color.FromRgb(255, 0, 0), Color.FromRgb(0, 255, 0),
             Color.FromRgb(0, 0, 255) };
         bool isTextMode = false;
 
         Client client;
+        bool online = false;
 
         public MainWindow()
         {
             AllocConsole();
-            TcpClient tcpClient = new TcpClient();
-            tcpClient.Connect("127.0.0.1", 5035);
-            client = new Client(tcpClient, doAddStroke, doSelectStroke, doDeselectStroke, doDeleteStroke, doModifStroke);
-            client.start();
+            if (online)
+            {
+                TcpClient tcpClient = new TcpClient();
+                tcpClient.Connect("127.0.0.1", 5035);
+                client = new Client(tcpClient, doAddStroke, doSelectStroke, doDeselectStroke, doDeleteStroke, doModifStroke);
+                client.start();
+            }
 
             InitializeComponent();
             inkCanvas.AddHandler(InkCanvas.MouseDownEvent, new MouseButtonEventHandler(clickCanvas), true);
@@ -47,7 +42,7 @@ namespace WhiteboardWPF
             penStyleBox.Items.Add("Pen");
             penStyleBox.Items.Add("Eraser");
 
-            for(int i = 0; i<availableColors.Count; i++)
+            for (int i = 0; i < availableColors.Count; i++)
             {
                 var textBlockColor = new TextBlock();
                 textBlockColor.Foreground = new SolidColorBrush(availableColors[i]);
@@ -62,24 +57,9 @@ namespace WhiteboardWPF
             inkCanvas.DefaultDrawingAttributes.StylusTip = System.Windows.Ink.StylusTip.Ellipse;
         }
 
-        
 
-        private void doAddStroke(int id, object o)
-        {
-            Dispatcher.Invoke(
-                () =>
-                {
-                    textBlock.Text = "Coucou";
-                    inkCanvas.Strokes.Add((Stroke)o);
-                }
-                );
-        }
-
-        private void doSelectStroke(int id) { }
-        private void doDeselectStroke(int id) { }
-        private void doDeleteStroke(int id) { }
-
-        private void doModifStroke(int id, object o) { }
+        // -----------------------------------------------------------------------------------------
+        // LOCAL CHANGES
 
         void selectedPenStyle(object sender, System.EventArgs e) //switch between pen and eraser
         {
@@ -101,27 +81,7 @@ namespace WhiteboardWPF
             }
         }
 
-        void clickEraseAllButton(object sender, System.EventArgs e)
-        {
-        }
-
-        void doEraseAll() // clear ink from canvas
-        {
-            inkCanvas.Strokes.Clear();
-        }
-
-        void strokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e) // collect last stroke collected
-        {
-            client.ask_add(e.Stroke);
-            inkCanvas.Strokes.Remove(e.Stroke);
-        }
-
-        void addNewStroke(System.Windows.Ink.Stroke newStroke)
-        {
-            inkCanvas.Strokes.Add(newStroke);
-        }
-
-        void sliderValueChanged(object sender, System.EventArgs e) // change width of stylus
+        void selectedStylusWidth(object sender, System.EventArgs e) // change width of stylus
         {
             inkCanvas.DefaultDrawingAttributes.Width = widthSlider.Value;
             inkCanvas.DefaultDrawingAttributes.Height = widthSlider.Value;
@@ -145,14 +105,88 @@ namespace WhiteboardWPF
             textBlock.Text = "HA";
             if (isTextMode)
             {
-                TextBlock newTextBlock = new TextBlock();
-                newTextBlock.Text = "Hello";
+                TextBlock newTextBlock = new TextBlock
+                {
+                    Text = "Hello"
+                };
                 inkCanvas.Children.Add(newTextBlock);
                 InkCanvas.SetTop(newTextBlock, e.GetPosition(this).Y);
                 InkCanvas.SetLeft(newTextBlock, e.GetPosition(this).X);
-                InkCanvas.GetLeft(newTextBlock);
             }
         }
+
+
+        // -----------------------------------------------------------------------------------------
+        // EVENTS SENT TO CLIENT
+
+        void strokeCollected(object sender, InkCanvasStrokeCollectedEventArgs e) // collect last stroke collected
+        {
+            if (online)
+            {
+                client.ask_add(e.Stroke);
+                inkCanvas.Strokes.Remove(e.Stroke);
+            }
+        }
+
+        void clickEraseAllButton(object sender, System.EventArgs e)
+        {
+        }
+
+
+        // -----------------------------------------------------------------------------------------
+        // FUNCTIONS CALLED FROM CLIENT
+
+        private void doAddStroke(int id, object o) // add a new stroke to canvas
+        {
+            Dispatcher.Invoke(
+                () =>
+                {
+                    textBlock.Text = "Coucou";
+                    inkCanvas.Strokes.Add((Stroke)o);
+                }
+                );
+        }
+
+        void doEraseAll() // clear ink from canvas
+        {
+            Dispatcher.Invoke(
+                () =>
+                {
+                    inkCanvas.Strokes.Clear();
+                });
+        }
+
+        void doAddTextBlock(string text, int x, int y) // add a new text block to canvas at specified position
+        {
+            Dispatcher.Invoke(
+                () =>
+                {
+                    TextBlock newTextBlock = new TextBlock
+                    {
+                        Text = text
+                    };
+                    inkCanvas.Children.Add(newTextBlock);
+                    InkCanvas.SetLeft(newTextBlock, x);
+                    InkCanvas.SetTop(newTextBlock, y);
+                });
+        }
+
+        private void doDeleteStroke(int id) // delete given stroke from canvas
+        {
+            Dispatcher.Invoke(
+                () =>
+                {
+                    inkCanvas.Strokes.Remove(inkCanvas.Strokes[id]);
+                });
+        }
+
+        private void doSelectStroke(int id) { }
+        private void doDeselectStroke(int id) { }
+        private void doModifStroke(int id, object o) { }
+
+
+        // -----------------------------------------------------------------------------------------
+        // CONSOLE
 
         [DllImport("Kernel32")] public static extern void AllocConsole();
 
