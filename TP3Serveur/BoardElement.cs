0563c7e8bd73d632c7441f36902b190499b277eb
Donatien de Coatpont <donatien.le-bescond-de-coatpont@student.ecp.fr>
2019-10-22 14:59:44 +0200

    Serveur plus propre

--- TP3Serveur/BoardElement.cs
+++ TP3Serveur/BoardElement.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace TP3Serveur
+namespace TCPServeur
 {
-    class BoardElement
+    abstract class BoardElement
     {
-        
+
+        public int m_id;
+        public object m_o;
+        public int m_clientLocker = -1;
+        public abstract string GetString();
     }
 }
---
+++ TP3Serveur/StrokeElement.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TCPServeur
+{
+    class StrokeElement : BoardElement
+    {
+        struct Point
+        {
+            public double x;
+            public double y;
+
+            public Point(double nx, double ny)
+            {
+                x = nx;
+                y = ny;
+            }
+        }
+
+        List<Point> lstPts = new List<Point>();
+        string Color;
+        bool FitToCurve;
+        double Height;
+        bool IgnorePressure;
+        bool IsHighlighter;
+        int StylusTip;
+        double Width;
+        public StrokeElement(int id, string locval)
+        {
+            string str = locval;
+            
+            string[] strlst = locval.Split('#');
+            string[] vals;
+            Color = strlst[0];
+            FitToCurve = Boolean.Parse(strlst[1]);
+            Height = Double.Parse(strlst[2]);
+            IgnorePressure = Boolean.Parse(strlst[3]);
+            IsHighlighter = Boolean.Parse(strlst[4]);
+            if (strlst[5] == "Rectangle")
+            {
+                StylusTip = 0;
+            }
+            else
+            {
+                StylusTip = 1;
+            }
+            Width = Double.Parse(strlst[7]);
+            str = strlst[8].TrimStart('%');
+            strlst = str.Split('%');
+            foreach (var coor in strlst)
+            {
+                if (coor is null)
+                {
+                    break;
+                }
+                else
+                {
+                    
+                    vals = coor.Split(';');
+                    lstPts.Add(new Point(Double.Parse(vals[0]), Double.Parse(vals[1])));
+                }
+
+            }
+
+
+        }
+        public override string GetString()
+        {
+            string locval;
+            locval = Color + "#" + FitToCurve.ToString() + "#" + Height.ToString() + "#" + IgnorePressure.ToString() + "#" + IsHighlighter.ToString() + "#" + StylusTip.ToString() + "#" + "garbage" + "#" + Width.ToString() + "#";
+            locval = locval.TrimStart('#');
+
+            foreach (Point point in lstPts)
+            {
+                locval += "%" + point.x + ";" + point.y;
+            }
+
+            return "str" + locval;
+        }
+    }
+}
